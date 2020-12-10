2743f9f0f526e6f1e5cd901cece6368bd8302833
MaximLyamin <maxim.lyamin@kaspersky.com>
2020-12-11 00:42:20 +0300

    test 3 lesson 1
    added test 3 lesson 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,4 +7,9 @@ public class MainClassTest extends MainClass {
     public void testGetClassNumber() {
         Assert.assertTrue("Class number is not more than 45",getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("Text doesnt contains 'hello' or 'Hello'", getClassString().contains("Hello")||getClassString().contains("hello"));
+    }
 }
