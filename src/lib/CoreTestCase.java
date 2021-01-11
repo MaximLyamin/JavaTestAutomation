4c4b1acd40d5d21c8f14e463ed40e179ece1e75d
MaximLyamin <maxim.lyamin@kaspersky.com>
2021-01-11 23:10:25 +0300

    added switch Platform
    Deleted iOSTestCase

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,27 +1,21 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
 
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+    protected Platform Platform;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
     }
 
@@ -42,28 +36,5 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
-    }
-
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "7.1.1");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/Users/lyamin_m/Desktop/JavaTestAutomation/apks/org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone 11 Pro");
-            capabilities.setCapability("platformVersion", "13.7");
-            capabilities.setCapability("app", "/Users/lyamin_m/Desktop/JavaTestAutomation/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-        return capabilities;
     }
 }
